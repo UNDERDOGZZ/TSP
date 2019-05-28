27636dedf14ea682633dc3a5694dce617f796de6
Luis Kcomt <45114652+kcomt@users.noreply.github.com>
2019-05-27 21:58:54 -0500

    Eleccion entre capitales

--- Archivos/kcomt.py
+++ Archivos/kcomt.py
@@ -2,24 +2,28 @@ import csv
 import copy
 import random as r
 
-def readFile(filename):
+def readFile(filename, numCapital):
     dictionary = {}
-
     with open(filename, encoding='utf-8-sig') as csvfile:
         file = csv.reader(csvfile, delimiter=',')
-        contador=0
+        contador=-1
         for row in file:
-                elem = {}
-                elem["id"]=row[0]
-                elem["departamento"] = row[1]
-                elem["provincia"] = row[2]
-                elem["distrito"] = row[3]
-                elem["codigo"] = row[4]
-                elem["x"] = float(row[15])
-                elem["y"] = float(row[16])
-                dictionary[contador] = elem
-                contador+=1
-                del elem
+                if contador > -1:
+                        if int(row[7]) == numCapital:
+                                elem = {}
+                                elem["id"]=row[0]
+                                elem["departamento"] = row[1]
+                                elem["provincia"] = row[2]
+                                elem["distrito"] = row[3]
+                                elem["codigo"] = row[4]
+                                elem["capital"] = int(row[7])
+                                elem["x"] = float(row[15])
+                                elem["y"] = float(row[16])
+                                dictionary[contador] = elem
+                                contador+=1
+                                del elem
+                else:
+                        contador+=1
     print(dictionary)
 
-readFile("CPPRUEBA.csv")
+readFile("CP_P_20180726.csv", 1)
